0f4e6cc9669b55a960858d4e28b0455bd1fc8a62
zAlweNy26 <zAlweNy26@users.noreply.github.com>
2023-10-18 11:22:30 +0000

    Build package

--- dist/index.mjs
+++ dist/index.mjs
@@ -1220,7 +1220,7 @@ var CatClient = class {
 };
 
 // index.ts
-var ccat_api_default = CatClient;
+var api_client_ts_default = CatClient;
 export {
   AcceptedMemoryTypes,
   AcceptedPluginTypes,
@@ -1229,6 +1229,6 @@ export {
   CancelablePromise,
   CatClient,
   WebSocketState,
-  ccat_api_default as default,
+  api_client_ts_default as default,
   isMessageResponse
 };
