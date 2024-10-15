3644575dc5d7c97c5d9252adf6e792a8113784b3
zAlweNy26 <zAlweNy26@users.noreply.github.com>
2024-10-15 20:57:57 +0000

    Build package

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -403,6 +403,18 @@ declare class PluginsService {
 
 type BodyUploadFile = {
     file: Blob;
+    /**
+     * Maximum length of each chunk after the document is split (in tokens)
+     */
+    chunk_size?: (number | null);
+    /**
+     * Chunk overlap (in tokens)
+     */
+    chunk_overlap?: (number | null);
+    /**
+     * Metadata to be stored with each chunk (e.g. author, category, etc.). Since we are passing this along side form data, must be a JSON string.
+     */
+    metadata?: string;
 };
 
 type BodyUploadMemory = {
